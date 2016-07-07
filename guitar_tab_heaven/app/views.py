8a9c4db6d5dcc35d51266a076708d7e64609eccd
koffskeyj <koffskeyj@gmail.com>
2016-07-06 21:01:18 -0400

    attempting pagination

--- guitar_tab_heaven/app/views.py
+++ guitar_tab_heaven/app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from urllib.parse import urlparse
 import requests
 from bs4 import BeautifulSoup
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def tab_detail_view(request, url):
@@ -10,15 +11,16 @@ def tab_detail_view(request, url):
     souper = BeautifulSoup(tab_result, "html.parser")
     tab = str(souper.find_all("pre"))
     stripped_tab = tab.strip("[]")
+
     return render(request, "detail.html", {"tab": tab, "stripped_tab": stripped_tab})
 
 def tab_search_view(request):
-    song_name = request.GET.get("song") or "Song "
+    song_name = request.GET.get("song") or "Smells Like Teen Spirit"
     url = "http://www.guitartabs.cc/search.php?tabtype=any&band=&song={}".format(song_name)
     results = requests.get(url).text
     souper = BeautifulSoup(results, "html.parser")
     links = [link["href"] for link in souper.find_all("a", class_="ryzh22")]
+    paging = souper.find_all(class_="paging")
     # print(results)
 
-
-    return render(request, "index.html", {"links": links})
+    return render(request, "index.html", {"links": links, "paging": paging})
